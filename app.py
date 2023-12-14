9f25eb743e17a25ec00c59e8292d55ecaf5b3b2a
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-12-14 18:49:34 +0530

    1st commit

---
+++ app.py
@@ -0,0 +1,52 @@
+
+import math
+
+
+# Type cpnversion
+
+# birth_year="1999"
+# print(type(birth_year))
+# age=2019-int(birth_year)
+# print(type(age))
+# print(age)
+
+# String
+# name="Rup Kumar Sarkar"
+
+# print(name[-1])
+
+# To get 0 to 5 chracter
+# print(name[0:5])
+
+# print(name.upper())
+# print(name.title())
+# print(name.find('u'))
+# print(name.replace('Sarkar','Das'))
+
+
+
+
+# Math Function
+
+# x=2.9
+# print(round(x))
+# print(math.floor(x))
+# print(abs(-5555))
+
+
+
+
+# if-else
+# x=2
+# if(x==2):
+#     print("true")
+# else:
+#     print("false")
+
+
+
+
+
+
+
+
