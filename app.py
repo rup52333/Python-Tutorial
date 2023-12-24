62df942a3cee926280d3fd7f90982777ec33ee7d
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-12-24 19:15:30 +0530

    4th commit

--- app.py
+++ app.py
@@ -81,4 +81,169 @@ import math
 # text="rup kumar"
 
 # print(text.find('r',text.find('r')+1))
+# i=1
+# while i!=10 :
+#     i=i+1
+#     print(i)
+
+# find median of three number
+
+
+
+
+
+
+# def biggestOfTwo(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+
+# def median(a, b, c):
+#     result = biggestOfTwo(a, b)
+
+#     if result > c:
+#         return c
+#     else:
+#         return result
+
+# print(median(2, 6, 8))
+
+
+# Given your Birthday and the current date,calculate your age in days
+
+# from datetime import datetime, date
+
+# def calculate_age(birth_year):
+#     # Get the current year
+#     current_year = date.today().year
+
+#     # Calculate the age
+#     age = current_year - birth_year
+
+#     return age
+
+# # Example usage
+# birth_year = int(input("Enter your birth year (YYYY): "))
+
+# age = calculate_age(birth_year)
+# print("Your age is:", age)
+
+
+# Define a procedure ,replace spy,that takes a its input a list of three numbers and modifies the vaue of the third elements in the input list to be one more than its previous value
+
+# spy=[0,0,7]
+# x=spy
+# spy[2]=x[2]+1
+# print (spy,x)
+
+#print su of numbers
+
+
+
+
+
+# def sum_list(p):
+#     sum = 0
+#     for e in p:
+#         sum += e
+#     return sum
+
+# p = [2, 5, 7, 8]
+# result = sum_list(p)
+# print(result)
+
+
+
+
+# pop operation
+
+# p=[2,6,5,4,8]
+# x=p.pop()
+# p.append(9)
+# print(x)
+# print(p)
+
+# web Scripping
+
+# step:
+# 1.Load webite
+# 2.Parst Html Data
+# 3.Extract tidy data
+# 4.Transform  data into required format
+
+
+# define procedure ,mae hashtabletht tkes as input a number  n buckets amnd outputs an empty hash table with n buckets empt uckets
+
+# def make_hashtable(nbuckets):
+#     i = 0
+#     table = []
+
+#     while i < nbuckets:
+#         table.append([])
+#         i = i + 1
+
+#     return table
+
+# print(make_hashtable(3))
+
+
+# def make_hashtable(nbuckets):
+#     table = []
+
+#     for i in range(nbuckets):
+#         table.append([])
+
+#     return table
+
+# print(make_hashtable(3))
+
+
+# index
+# l=[29,45,6,7,4]
+
+# print(l.index(7))
+
+
+# Dictionary
+
+# elements={}
+
+# elements['A']={'name':'Apple','wt':'2kg'}
+# elements['O']={'name':'Orange','wt':'6kg'}
+# print(elements['A']['name']+">"+elements['A']['wt'])
+
+
+# check palindrome
+
+# def isPalindrome(s):
+#     n = len(s)
+#     l = 0
+
+#     if s == "":
+#         return True
+#     else:
+#         if s[0] == s[n-1]:
+#             return isPalindrome(s[l+1:n-1])
+#         else:
+#             return False
+
+# print(isPalindrome("acccdcca"))
+
+
+
+# fibonacci number
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fib(n - 1) + fib(n - 2)
+
+# print(fib(5))
+
+
+
+
 
